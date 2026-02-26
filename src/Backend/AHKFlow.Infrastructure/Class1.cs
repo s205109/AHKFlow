f59e44ae397a5e8590b0407f97a3715f5751c629
Bart Segers (M16) <btasegers@live.nl>
2026-02-26 14:19:15 +0100

    Updated NuGet packages + code cleanup

--- src/Backend/AHKFlow.Infrastructure/Class1.cs
+++ src/Backend/AHKFlow.Infrastructure/Class1.cs
@@ -1,4 +1,7 @@
-﻿namespace AHKFlow.Infrastructure
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace AHKFlow.Infrastructure
 {
     public class Class1
     {
--- tests/AHKFlow.Infrastructure.Test/UnitTest1.cs
+++ tests/AHKFlow.Infrastructure.Test/UnitTest1.cs
@@ -1,4 +1,7 @@
-﻿namespace AHKFlow.Infrastructure.Test
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace AHKFlow.Infrastructure.Test
 {
     public class UnitTest1
     {
