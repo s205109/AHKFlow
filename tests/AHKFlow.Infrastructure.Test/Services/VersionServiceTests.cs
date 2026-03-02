8360ebd23d1e8b1ad58c9bdabf758f165bdec674
Bart Segers (M16) <btasegers@live.nl>
2026-03-02 11:48:58 +0100

    Updated VersionServiceTests

--- tests/AHKFlow.Infrastructure.Test/Services/VersionServiceTests.cs
+++ tests/AHKFlow.Infrastructure.Test/Services/VersionServiceTests.cs
@@ -6,13 +6,13 @@ namespace AHKFlow.Infrastructure.Tests.Services
     public class VersionServiceTests
     {
         [Fact]
-        public async Task GetVersionAsync_ShouldReturnVersion()
+        public async Task GetVersionAsync_ShouldReturnNonEmptyVersion_WhenAssemblyVersionIsAvailable()
         {
             // Arrange
             var service = new VersionService();
 
             // Act
-            string version = await service.GetVersionAsync();
+            string version = await service.GetVersionAsync(CancellationToken.None);
 
             // Assert
             version.Should().NotBeNullOrEmpty();
